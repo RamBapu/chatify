4fff05924dc60b80b6dc9848c71e9c95bce31abc
Ram Bapu <rambapu270197@gmail.com>
2026-01-29 17:39:12 +0530

    deployment code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
@@ -6,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT;
 
@@ -14,5 +16,14 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+
+// deployment code
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log("Server started on port", PORT));

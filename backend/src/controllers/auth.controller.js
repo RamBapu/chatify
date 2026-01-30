04996a5e6f7850c3c9016774bec7054c01739efc
Ram Bapu <rambapu270197@gmail.com>
2026-01-30 19:01:41 +0530

    feat: add login and logout functionality to authentication routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -63,3 +63,37 @@ export const signUp = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: "User doesnt exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ error: "Invalid credentials" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in login controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = async (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { login, logout, signUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;

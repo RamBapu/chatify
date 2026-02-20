5840f899932fe22867df0f93ae359f6a8598ee14
Ram Bapu <rambapu270197@gmail.com>
2026-02-20 13:19:11 +0530

    feat: add welcome message and API endpoints to root route in server.js; update .gitignore to include DEV_README.md

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,6 +18,16 @@ app.use(express.json({ limit: "5mb" })); // Parses json from request body
 app.use(cookieParser()); // To extract userId from cookies
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // allow request from frontend with cookies
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Chatify API - Powered by Mongoose & MongoDB",
+    endpoints: {
+      users: "/api/users",
+      messages: "/api/messages",
+    },
+  });
+});
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Health check passed" });
 });
@@ -26,13 +36,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // deployment code
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("Server started on port", PORT);

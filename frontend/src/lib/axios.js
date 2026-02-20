fe408a5712d413b9957eb2eca2ccd75c8f345dde
Ram Bapu <rambapu270197@gmail.com>
2026-02-20 13:27:04 +0530

    feat: update axios instance baseURL to use VITE_API_URL for consistent API endpoint configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // send the request to same url with /api if both frontend and backend are deployed in the same domain
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
 
   withCredentials: true, // send the cookies along with the request body
 });
